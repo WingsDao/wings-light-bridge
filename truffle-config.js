3d3c599af75708c8d42e126ea8b1848987247949
romfrolov <roman.frolov@wings.ai>
2018-05-07 12:16:39 +0300

    config fix

--- truffle-config.js
+++ truffle-config.js
@@ -16,9 +16,8 @@ module.exports = {
   contracts_build_folder: './build',
   networks: {
     development: {
-      provider: () => {
-        return new HDWalletProvider(account.privateKey, PROVIDER + API_TOKEN)
-      },
+      host: 'localhost',
+      port: 8545,
       network_id: '*'
     },
     mainnet: {
