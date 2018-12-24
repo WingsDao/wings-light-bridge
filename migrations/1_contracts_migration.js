e372afbd97fce31f3a5424cbaf5c05f984cdda07
@romfrolov <roman.frolov@wings.ai>
2018-12-24 10:05:51 +0300

    Fixed migration file.

--- migrations/1_contracts_migration.js
+++ migrations/1_contracts_migration.js
@@ -4,13 +4,11 @@
 const Bridge = artifacts.require('Bridge');
 
 module.exports = (deployer) => {
-    // deployer.then(async () => {
-    //     const creator = '0xB30Ec237c916473301572e6c9ddBd0189D5F2d61';
-    //
-    //     await deployer.deploy(Bridge, creator, creator, {gas: 4700000});
-    //
-    //     let bridge = await Bridge.deployed();
-    //
-    //     console.log(bridge.address);
-    // });
+    deployer.then(async () => {
+        if (process.env.CREATOR) {
+            const creator = process.env.CREATOR;
+
+            await deployer.deploy(Bridge, creator, creator, {from: creator, gas: 4700000});
+        }
+    });
 };
